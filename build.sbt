1543f4108d22285d50bc2614dd327c419b330a2c
toriving <toriving@gmail.com>
2020-05-20 00:36:15 +0900

    Updated allaboutscala

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "allaboutscala"
+
+version := "0.1"
+
+scalaVersion := "2.12.1"
---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_04/HelloWorld.scala
@@ -0,0 +1,5 @@
+package com.allaboutscala.chapter.one.tutorial_04
+
+object HelloWorld extends App{
+  println("Hello World from Scala!")
+}
